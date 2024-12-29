05561f2a49bda0278ff1fa71b95e4b9c5cc8d1b0
shaker113 <shaker.baker113@gmail.com>
2024-12-29 09:47:46 +0300

    (feat) update Dockerfile and docker-compose for improved build process and add shared network

--- set_environment.sh
+++ set_environment.sh
@@ -5,6 +5,7 @@ echo "Setting up .env file for the project...
 By default, the current working directory will be used as the BOTS_PATH and the CONFIG_PASSWORD will be set to 'a'."
 
 # Asking for CONFIG_PASSWORD and BOTS_PATH
+# should be pushed to github 
 CONFIG_PASSWORD=a
 USERNAME=admin
 PASSWORD=admin
