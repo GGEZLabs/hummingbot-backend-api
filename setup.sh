2a27f2d74ed98c978c5d77adf5705242043ce5b1
shaker113 <shaker.baker113@gmail.com>
2025-08-20 11:14:42 +0300

    comment out hummingbot image docker pull command in setup script

--- setup.sh
+++ setup.sh
@@ -150,7 +150,7 @@ echo -e "${GREEN}🐳 Starting required Docker containers and pulling Hummingbot
 
 # Run docker operations in parallel
 docker compose up emqx postgres -d &
-docker pull hummingbot/hummingbot:latest &
+# docker pull hummingbot/hummingbot:latest &
 
 # Wait for both operations to complete
 wait
